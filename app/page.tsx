7f4b9cf2e9253982e10c2bd09e84e92aebfdd892
NeterNFT <104866216+neternnft@users.noreply.github.com>
2024-06-29 18:22:41 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
-      <p className="fixed left-0 top-0 flex w-full justify-start  from-zinc-200 pb-6 pt-8">
-          <code className="font-mono font-bold text-white px-5 py-3">DO</code>
+      <p className="fixed left-0 top-0 flex w-full justify-start items-center from-zinc-200 pb-6 pt-8">
+        <code className="font-mono font-bold text-white px-10 py-5 text-4xl">DO</code>
       </p>
 
       {/* Yoda Image Centered */}
@@ -18,18 +18,19 @@ export default function Home() {
           priority
         />
       </div>
+
       {/* Dextools Link */}
       <a
-          className="group"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-lg font-semibold text-white">
-            MEME IS A GAME{" "}
-            <span className="inline-block">
-            </span>
-          </h2>
-        </a>
+        className="group"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <h2 className="mb-3 text-lg font-semibold text-white ">
+          MEME IS A GAME{" "}
+          <span className="inline-block"></span>
+        </h2>
+      </a>
+
       {/* Links Section */}
       <div className="mb-32 grid gap-4 lg:grid-cols-4 lg:max-w-5xl lg:text-left">
         {/* X Link */}
