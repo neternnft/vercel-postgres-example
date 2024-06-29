19f841e428e91ce61357915ad3f9fe31f92046b3
NeterNFT <104866216+neternnft@users.noreply.github.com>
2024-06-29 05:05:51 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
-      <div className="z-10 max-w-10xl items-center justify-between lg:flex">
-        <p className="fixed right-20 top-5 flex  justify-center bg-gradient-to-b from-zinc-50 dark:from-inherit lg:static lg:w-auto">         
+      <div className="z-10 max-w-10xl justify-between lg:flex">
+        <p className="right-20 top-5 flex  justify-center bg-gradient-to-b from-zinc-50 lg:static lg:w-auto">         
           <Image
             className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
             src="/DoLabs.svg"
