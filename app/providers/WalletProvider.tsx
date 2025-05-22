a0a9e2c6515a83ea020bf67099fcd1d1fde8d161
NeterNFT <netern.nft@gmail.com>
2025-05-23 00:11:43 +0300

    a

--- app/providers/WalletProvider.tsx
+++ app/providers/WalletProvider.tsx
@@ -1,34 +1,33 @@
 'use client';
 
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
 import { avalanche } from 'wagmi/chains';
-import { publicProvider } from 'wagmi/providers/public';
+import { http } from 'viem';
 import '@rainbow-me/rainbowkit/styles.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const { chains, publicClient } = configureChains(
-  [avalanche],
-  [publicProvider()]
-);
+const projectId = '75a8d6457e30a6239cf7ec6b23de3e9c';
 
-const { connectors } = getDefaultWallets({
+const queryClient = new QueryClient();
+
+const config = getDefaultConfig({
   appName: 'Glurbnok',
-  projectId: '75a8d6457e30a6239cf7ec6b23de3e9c',
-  chains,
-});
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
+  projectId,
+  chains: [avalanche],
+  transports: {
+    [avalanche.id]: http()
+  }
 });
 
 export function WalletProvider({ children }: { children: React.ReactNode }) {
   return (
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
-        {children}
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 } 
